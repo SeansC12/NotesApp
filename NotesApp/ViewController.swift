cbe92db77274497a77c8929b1f232f62c3f0c6e5
SeansC12 <88256324+SeansC12@users.noreply.github.com>
2021-10-31 18:27:03 +0800

    Initial Commit

---
+++ NotesApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NotesApp
+//
+//  Created by SEAN ULRIC BUGUINA CHUA stu on 31/10/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
